ab9d8e7c2d2f7181ba40928b7e2a6f8548d7e110
Fred <pf.robert974@gmail.com>
2024-01-18 16:47:15 +1100

    registering event listener

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+function startGame() {
+  console.log('Gmae is starting...');
+}
+
+startGameBtn.addEventListener('click', startGame);
