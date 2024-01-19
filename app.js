53b743bfb28fa00813ad2fceecd6467825b13d45
Fred <pf.robert974@gmail.com>
2024-01-19 15:55:57 +1100

    added more logic

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER WINS';
 
 let gameIsRunning = false;
 
@@ -20,6 +23,31 @@ const getPlayerChoice = function() {
     return selection;
 };
 
+const getComputerChoice = function() {
+  const randomValue = Math.random();
+  if (randomValue < 0.33) {
+    return ROCK;
+  } else if (randomValue < 0.66) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
+
+const getWinner = function(cChoice, pChoice) {
+  if (cChoice === pChoice) {
+    return RESULT_DRAW;
+  } else if (
+    cChoice === ROCK && pChoice === PAPER ||
+    cChoice === PAPER && pChoice === SCISSORS ||
+    cChoice === SCISSORS && pChoice === ROCK
+    ) {
+      return RESULT_PLAYER_WINS;
+    } else {
+      return RESULT_COMPUTER_WINS;
+    }
+};
+
 startGameBtn.addEventListener('click', function() {
   if (gameIsRunning) {
     return;
@@ -27,5 +55,7 @@ startGameBtn.addEventListener('click', function() {
   gameIsRunning = true;
   console.log('Game is starting...');
   const playerSelection = getPlayerChoice();
-  console.log(playerSelection);
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(computerChoice, playerSelection)
+  console.log(winner);
 });
